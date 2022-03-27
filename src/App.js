d4fce8e6aac470c88227b8720ebcb94cd2bb3efe
“Jason <jasonrodalez@gmail.com>
2022-03-27 10:52:39 -0500

    Subida a github de la Quote-App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './components-css/App.css';
+import CardQuotes from './components/CardQuotes';
+import Quotes from './Quotes.json'
+import { useState } from 'react';
 
 function App() {
+
+  // Declaración de los colores a utilizar en el boton y fondo de la app-quote
+  let colors = ["Blue", "Grey", "Green", "Violet", "Orange", "Red"];
+
+  //Declaración del ramdom empleado en los colores  de la app-quote
+  let randomColor = Math.floor((Math.random() * 4) + 1);
+
+  //Declaración del ramdom empleado en la quote-app para las citas
+  let randomQuote = Math.floor((Math.random() * 101) + 1);
+
+  //El random del estado inicial de la app
+  let randomQuoteState = Math.floor((Math.random() * 20) + 1);
+
+  //Declaración del estado inicial de la quote-app
+  const [quote, setQuote] = useState(Quotes.quotes[randomQuoteState]);
+
+
+  //Declaración de la funcion principal dela quote-app.Indica el ramdon respectivo.
+  const getCitas = () => {
+    setQuote(Quotes.quotes[randomQuote]);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      {/* En el  header se declara una clase dinámica, que está concatenada con el ramdon de colores */}
+      <header className={`App-header-${colors[randomColor]}`}>
+        <div className="card-quotes">
+          
+          {/* Se declara el llamado al setQuote. Será recibido por el componente, en donde se desglkosan las respectivas propiedades: auto, cita, id  */}
+          <CardQuotes
+            quotes={quote}
+          />
+
+          {/* En el  botton  se declara una clase dinámica, que 
+          está concatenada con el ramdon de colores. El onclick dirige a
+           la funciónrespectiva */}
+          <button className={`App-button-${colors[randomColor]}`} onClick={() => getCitas()}>Next</button>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/CardQuotes.js
@@ -0,0 +1,19 @@
+import '../components-css/CardQuotes.css';
+const CardQuotes = ({ quotes }) => {
+
+    return (
+        <div >
+            <h2>{quotes.id}</h2>
+            <h2>{quotes.autor}</h2>
+            <blockquote className="card-quotes__blockquote">{quotes.cita}</blockquote>
+            <a href="https://twitter.com/intent/tweet?text=Te%20comparto%20mi%20Quote%20app&url=http%3A%2F%2Freact.me%2Fblog&via=parzibyte&hashtags=programación,react" target="_blank" className="card-quotes__ico-twiter"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+            </svg></a>
+        </div>
+    )
+
+
+}
+
+export default CardQuotes;
